b52b8986a1cec48304bc09a953f1c0f8b8b24310
lucy0511d <luxiaagzl1105@gmail.com>
2024-10-17 21:08:11 +0200

    rrrrr

---
+++ Assets/Scripts/FirstPerson.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPerson : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // no sirve: Vector3 movimiento = new Vector3(h, 0, v).normalized;
+
+        Vector2 input = new Vector2 (h, v).normalized;
+        if (input.magnitude > 0)
+        {
+            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+
+            Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
+
+            controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+        }
+        
+        
+
+    }
+}
